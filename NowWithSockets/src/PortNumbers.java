fa25d332932274de6dbfbd94201c3ed3752cd28b
Menolo-Homobovanez <118155256+MenachemJacobs@users.noreply.github.com>
2024-10-21 13:50:15 -0400

    Trying A New Slave System

---
+++ NowWithSockets/src/PortNumbers.java
@@ -0,0 +1,5 @@
+public class PortNumbers {
+    public static final int MasterPort = 200;
+    public static final int ASlavePort = 201;
+    public static final int BSlavePort = 202;
+}
---
+++ NowWithSockets/src/Task.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+
+public class Task implements Serializable{
+    int taskID;
+    int clientID;
+    TaskType taskType;
+
+    Task(int taskNum, int clientNum, TaskType type){
+        taskID = taskNum;
+        clientID = clientNum;
+        taskType = type;
+    }
+
+    void efficientExecute() {
+        try{
+            Thread.sleep(2 * 1000);
+        } catch(InterruptedException ignored){}
+    }
+
+    void inefficientExecute() {
+        try{
+            Thread.sleep(5 * 1000);
+        } catch(InterruptedException ignored){}
+    }
+}
+
+enum TaskType{
+    A,
+    B
+}
